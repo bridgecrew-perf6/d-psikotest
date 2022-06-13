679de761e2c49afd13164ba7e1a67d5de37d31bb
godgodwinter <kakadz.pro@gmail.com>
2022-06-13 23:12:19 +0700

    deploy 13-6-22

---
+++ owner/assets/ButtonKembali.1603ddd4.js
@@ -0,0 +1,1 @@
+import{b as o,o as s,e as a,h as n,f as r}from"./index.3c250083.js";const _=r("button",{class:"btn hover:shadow-lg shadow text-white hover:text-gray-100 gap-2"}," Kembali ",-1),c=[_],h={__name:"ButtonKembali",setup(u){const t=o();return(i,e)=>(s(),a("span",{onClick:e[0]||(e[0]=l=>n(t).go(-1))},c))}};export{h as _};
